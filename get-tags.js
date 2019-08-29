000d053b1d1a12367121c0a8806587c31d320d41
nakedsushi <louise.m.yang@gmail.com>
2019-08-28 22:20:49 -0700

    Adds script to generate tags

---
+++ get-tags.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+
+const nonTags = ['lunch', 'breakfast', 'dinner', 'low', 'medium', 'high'];
+const response = JSON.parse(fs.readFileSync('tags.json'));
+
+const tags = [];
+response.Items.forEach((item) => {
+  item.tags.L.forEach((tag) => {
+    tags.push(tag.S);
+  });
+});
+
+const uniqTags = Array.from(new Set(tags));
+
+console.log(uniqTags.filter(tag => nonTags.indexOf(tag) < 0 ));
