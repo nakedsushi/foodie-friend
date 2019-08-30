9575912ea90699feace2b43f62f53746201c88e3
nakedsushi <louise.m.yang@gmail.com>
2019-08-30 15:30:19 -0700

    Shortens deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,8 @@
 
 # abort on errors
 set -e
-
-# build
 yarn build
-
-# navigate into the build output directory
 cd dist
-
 aws s3 cp ./ s3://$BUCKET_NAME/ --recursive
-aws cloudfront create-invalidation --distribution-id=$CLOUDFRONT_DIST_ID --paths *
-
+aws cloudfront create-invalidation --distribution-id=$CLOUDFRONT_DIST_ID --paths "/*"
 cd -
